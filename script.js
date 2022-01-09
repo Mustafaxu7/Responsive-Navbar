5bd9d07b7c226a58e8cc27992db6adaa57b4a376
Mustafa Unal <94565017+MustafaxUK@users.noreply.github.com>
2022-01-09 21:05:17 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict"
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+navToggle.addEventListener("click", function () {
+    console.log(links.classList);
+    console.log(links.classList.contains("hex"))
+    console.log(links.classList.contains("links"));
+
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links")
+    }
+    else {
+        links.classList.toggle("show-links")
+    }
+})
